9c27e7e8a568432fa4e82aecae640b3fb7df6ed0
DTengen <david_tengbjork@hotmail.com>
2015-04-10 11:16:31 +0200

    Uppdaterade vacation och verification repositories
    Uppdaterade vacation och verificatoin repositories

--- vITs/DAL/VacationsRepository.cs
+++ vITs/DAL/VacationsRepository.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace DAL
 {
-    class VacationsRepository
+    public class VacationsRepository
     {
         DatabaseEntities _dbConnect = new DatabaseEntities();
 
--- vITs/DAL/VerificationRepository.cs
+++ vITs/DAL/VerificationRepository.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace DAL
 {
-    class VerificationRepository
+    public class VerificationRepository
     {
         DatabaseEntities _dbConnect = new DatabaseEntities();
 
