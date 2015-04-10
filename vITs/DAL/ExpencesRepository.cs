8c41832cc91d21450d0bd8273e095aad26589feb
DTengen <david_tengbjork@hotmail.com>
2015-04-10 12:25:05 +0200

    Skapade expence repository
    Skapade expence repository

---
+++ vITs/DAL/ExpencesRepository.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Remoting.Contexts;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL
+{
+    public class ExpencesRepository
+    {
+        public static void addExpence(Expence expence)
+        {
+            using (var context = new DatabaseEntities())
+            {
+                context.Expences.Add(expence);
+                context.SaveChanges();
+            }
+        }
+
+        public static void deleteExpence(Expence expence)
+        {
+            using (var context = new DatabaseEntities())
+            {
+                Expence e = context.Expences.FirstOrDefault(x => x.expenceID == expence.expenceID);
+                context.Expences.Remove(e);
+                context.SaveChanges();
+            }
+        }
+
+        public static void deleteExpence(int expenceID)
+        {
+            using (var context = new DatabaseEntities())
+            {
+                Expence e = context.Expences.FirstOrDefault(x => x.expenceID == expenceID);
+                context.Expences.Remove(e);
+                context.SaveChanges();
+            }
+        }
+    }
+}
