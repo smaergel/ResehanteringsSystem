042aa1651b5149d2c59b5b30e69c30cf2a66b274
oskary <oskaryvell@gmail.com>
2015-04-20 15:58:57 +0200

    Fixade en j'vla massa skit

---
+++ vITs/vITs/Logic/Serializer.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using vITs.Models;
+
+namespace vITs.Logic
+{
+    public static class Serializer
+    {
+        
+        public static void Save(FullTrip fullTrip)
+        {
+            var Filename = "bajshog.xml";
+            var writer = new System.Xml.Serialization.XmlSerializer(typeof(FullTrip));
+
+            var file = new System.IO.StreamWriter(Filename);
+            writer.Serialize(file, fullTrip);
+            file.Close();
+        }
+    }
+}
